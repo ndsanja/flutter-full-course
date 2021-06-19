aaf255ca3c0097410294776fe084a25f2d9a1388
ndsanja <ndsanja@gmail.com>
2021-06-19 21:49:14 +0700

    commit main branch hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/ExtractWidget.dart';
+import './widgets/HelloWorld.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ExtractWidget(),
+      home: HelloWorld(),
     );
   }
 }
