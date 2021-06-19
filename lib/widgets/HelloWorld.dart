e22da36506e5138d677a0a4678fb06bcc2cca742
ndsanja <ndsanja@gmail.com>
2021-06-19 22:43:58 +0700

    11-statefull-widget

--- lib/widgets/HelloWorld.dart
+++ lib/widgets/HelloWorld.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 
-class HelloWorld extends StatelessWidget {
+class HelloWorld extends StatefulWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  _HelloWorldState createState() => _HelloWorldState();
+}
+
+class _HelloWorldState extends State<HelloWorld> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello World"),
       ),
-      body: Center(child: Text("Hello World")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 20 + double.parse(counter.toString())),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (counter != 1) {
+                      setState(() {
+                        counter--;
+                      });
+                    }
+                  },
+                  child: Icon(Icons.remove),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
